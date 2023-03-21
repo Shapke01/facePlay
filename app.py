032ce62661871bd67608d7a855865842dbd53087
Shapke01 <stanislaw.hapke@gmail.com>
2023-03-21 17:03:43 +0100

    streamlit

--- app.py
+++ app.py
@@ -1,21 +1,85 @@
 import cv2
 import streamlit as st
-
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
+import numpy as np
+from PIL import Image
 
 
 
+st.set_page_config(layout="wide")
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+# TITLE
+st.markdown("<h1 style='text-align: center; color: white;'>FACE APP</h1>", unsafe_allow_html=True)
+st.markdown("""---""")
+new_faces = st.sidebar.file_uploader(label = "Add face image :blush: (preffered size: 600x400)",
+                                accept_multiple_files = True)
+
+faces = list()
+names = list()
+
+for face in new_faces:
+    names.append(face.name.split('.')[0])
+    faces.append(cv2.resize(np.array(Image.open(face)), (600,400)))
+
+cols = st.columns(max(len(faces),1))
+sliders = [None] * len(faces)
+if len(faces) > 0:
+    for idx, (face, col) in enumerate(zip(faces, cols)):
+        with col:
+            st.markdown(f"<p style='text-align: center; color: white;'>{names[idx]}</p>", unsafe_allow_html=True)
+            st.image(face)
+            sliders[idx] = st.slider(str(idx), 0., 1., 0.01, label_visibility='collapsed')
 
 
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# run = st.checkbox('Run')
+# FRAME_WINDOW = st.image([])
+# camera = cv2.VideoCapture(0)
+
+# while run:
+#     _, frame = camera.read()
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
 
     
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+#     FRAME_WINDOW.image(frame)
+# else:
+#     st.write('Stopped')
